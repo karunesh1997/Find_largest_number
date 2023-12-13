486ae547c4d4980a2808a16e003fb402199448f6
karunesh chaturvedi <57280336+karunesh1997@users.noreply.github.com>
2023-12-13 20:56:59 +0530

    Create largest_number.py

---
+++ largest_number.py
@@ -0,0 +1,12 @@
+def find_largest(num1,num2,num3):
+  return max(number1, number2, number3)
+
+st.title('Find the Largest Number')
+
+number1 = st.number_input("Insert first number", value=None, placeholder="Type a number...")
+number2 = st.number_input("Insert second number", value=None, placeholder="Type a number...")
+number3 = st.number_input("Insert third number", value=None, placeholder="Type a number...")
+
+if st.button('Find Largest'):
+  largest = find_largest(number1, number2, number3)
+  st.success('The largest number is: ', largest)
